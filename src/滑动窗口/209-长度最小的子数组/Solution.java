bbc00ebd27cedf3b50dbc8f4d7a13099bb7521cc
songyx <2250794569@qq.com>
2024-03-31 12:21:56 +0800

    [ADD] 滑动窗口$$栈(2+3)

---
+++ src/滑动窗口/209-长度最小的子数组/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public static int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        int start = 0, end = 0;
+        int sum = 0;
+        while (end < n) {
+            sum += nums[end];
+            while (sum >= target) {
+                ans = Math.min(ans, end - start + 1);
+                sum -= nums[start];
+                start++;
+            }
+            end++;
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,4,5,6};
+        int minSubArrayLen = minSubArrayLen(9, nums);
+        System.out.println(minSubArrayLen);
+    }
+}
