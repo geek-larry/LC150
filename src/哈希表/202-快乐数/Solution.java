bc4bd75f027350021e38e207f422b6f305dbd773
Larry <songyx0303@126.com>
2024-05-07 21:29:22 +0800

    [ADD] 哈希表3

---
+++ src/哈希表/202-快乐数/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public static int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+    public static boolean isHappy(int n) {
+        int slowRunner = n;
+        int fastRunner = getNext(n);
+        while (fastRunner != 1 && slowRunner != fastRunner) {
+            slowRunner = getNext(slowRunner);
+            fastRunner = getNext(getNext(fastRunner));
+        }
+        return fastRunner == 1;
+    }
+
+    public static void main(String[] args) {
+        int i = 19;
+        boolean happy = isHappy(i);
+        System.out.println(happy);
+    }
+}
