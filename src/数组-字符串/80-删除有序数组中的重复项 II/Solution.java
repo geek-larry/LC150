4d34f817c0bac92e217d72c4bb72d5cc5e493a88
Larry <songyx0303@126.com>
2024-05-21 20:57:32 +0800

    [ADD] 删除有序数组中的重复项 II

---
+++ src/数组-字符串/80-删除有序数组中的重复项 II/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public static int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n <= 2) {
+            return n;
+        }
+        int slow = 2, fast = 2;
+        while (fast < n) {
+            if (nums[slow - 2] != nums[fast]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,2,2,3};
+        int removeDuplicates = removeDuplicates(nums);
+        System.out.println(removeDuplicates);
+    }
+}
