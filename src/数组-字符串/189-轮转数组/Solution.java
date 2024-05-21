0291829bd13661a414fbddd599148df3b735c9fd
Larry <songyx0303@126.com>
2024-05-21 21:06:59 +0800

    [ADD] 轮转数组

---
+++ src/数组-字符串/189-轮转数组/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start += 1;
+            end -= 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+        rotate(nums, 4);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+}
