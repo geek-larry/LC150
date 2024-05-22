1085b54fdce0d211b1457288d847fd4d0edde1a4
Larry <songyx0303@126.com>
2024-05-22 22:39:01 +0800

    [ADD] 最后一个单词的长度

---
+++ src/数组-字符串/58-最后一个单词的长度/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static int lengthOfLastWord(String s) {
+        int index = s.length() - 1;
+        while (s.charAt(index) == ' ') {
+            index--;
+        }
+        int wordLength = 0;
+        while (index >= 0 && s.charAt(index) != ' ') {
+            wordLength++;
+            index--;
+        }
+        return wordLength;
+    }
+
+    public static void main(String[] args) {
+        String s = "   fly me   to   the moon  ";
+        int lengthOfLastWord = lengthOfLastWord(s);
+        System.out.println(lengthOfLastWord);
+    }
+}
