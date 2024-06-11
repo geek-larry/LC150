05f9e3b811609aab116f9e25f3073a04f03f6803
larry <songyx0303@126.com>
2024-06-11 13:27:52 +0800

    [ADD] 哈希表

---
+++ src/哈希表/219-存在重复元素 II/Solution.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            int num = nums[i];
+            if (map.containsKey(num) && i - map.get(num) <= k) {
+                return true;
+            }
+            map.put(num, i);
+        }
+        return false;
+    }
+}
