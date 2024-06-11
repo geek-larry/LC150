cffca898d35adaff062144790a0bac516b4137da
larry <songyx0303@126.com>
2024-06-11 17:24:25 +0800

    [FIX] 加一

--- src/数学/66-加一/Solution.java
+++ src/数学/66-加一/Solution.java
@@ -1,3 +1,10 @@
+/**
+ * 
+ * 链接：https://leetcode.cn/problems/plus-one/solutions/4481/java-shu-xue-jie-ti-by-yhhzw/
+ * 
+ * @author Larry
+ * @since 2024-06-11 17:23:41
+ */
 class Solution {
     public int[] plusOne(int[] digits) {
         int len = digits.length;
